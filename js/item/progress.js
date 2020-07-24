319997f7217a898fef1dc8b2464d5a87137c6a6e
jaseo <sjungah99@gmail.com>
2020-07-24 16:48:39 +0900

    skill graph !!!!!!

--- js/item/progress.js
+++ js/item/progress.js
@@ -10,8 +10,19 @@ const loadProgress = async () => {
     const skill = document.querySelector("#skill");
     skill.innerHTML = await fetchHtmlAsText("./skill.html");
 
+    const svgLoad = [
+        { select: ".pro_90", percent: '90%', front: "M6,48A44,44,0,0,1,92,34.83" },
+        { select: ".pro_80", percent: '80%', front: "M6,48A44,44,0,0,1,86.39,23.26" },
+        { select: ".pro_70", percent: '70%', front: "M6,48A44,44,0,0,1,76.35,12.76" },
+        { select: ".pro_60", percent: '60%', front: "M6,48A44,44,0,0,1,64.49,6.44" },
+        { select: ".pro_50", percent: '50%', front: "M6,48A44,44,0,0,1,50,4" }
+    ];
+
     const progress = ({ select, percent, front }) => {
-        document.querySelector(select).innerHTML =
+        const a = document.querySelectorAll(select);
+
+        a.forEach((value) => {
+            value.innerHTML =
             '<div>' +
             '<svg svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50"  class="front" >' +
             `<path d=${front}></path> ` +
@@ -21,15 +32,10 @@ const loadProgress = async () => {
             '</svg>' +
             `<p>${percent}</p>` +
             ' </div>';
+        });
     };
 
-    const svgLoad = [
-        { select: ".pro_90", percent: '90%', front: "M6,48A44,44,0,0,1,92,34.83" },
-        { select: ".pro_80", percent: '80%', front: "M6,48A44,44,0,0,1,86.39,23.26" },
-        { select: ".pro_70", percent: '70%', front: "M6,48A44,44,0,0,1,76.35,12.76" },
-        { select: ".pro_60", percent: '60%', front: "M6,48A44,44,0,0,1,64.49,6.44" },
-        { select: ".pro_50", percent: '50%', front: "M6,48A44,44,0,0,1,50,4" }
-    ];
+    svgLoad.forEach((value, index) => (progress({ ...value })));
     svgLoad.forEach((value, index) => (progress({ ...value })));
 
 };
