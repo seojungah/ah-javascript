1b7dc5216a5450a321cb010618ad6c0099753db8
jaseo <sjungah99@gmail.com>
2020-07-31 11:04:33 +0900

    2020-07-31

--- js/item/list.js
+++ js/item/list.js
@@ -10,16 +10,22 @@ const makeList = async () => {
     const main = document.querySelector("#main");
     const closeBtn = document.querySelector(".close_btn");
     const section = document.querySelector("section");
+    const closeMain = document.getElementsByClassName("close");
 
     listItem.forEach((value) => {
         //     const hiddenPage = document.querySelector(`#list_${index + 1}`);
         value.addEventListener('click', () => {
-            main.classList.toggle('close');
-            closeBtn.classList.toggle('close');
-            section.classList.toggle('close');
-            //         hiddenPage.classList.toggle('unfold');
+            if (closeMain.length > 0) {
+                main.classList.toggle('close');
+                closeBtn.classList.toggle('close');
+                section.classList.toggle('close');
+            }
+            main.style.backgroundColor = '#e3c3c2';
         });
     });
+
+
+
 };
 
 makeList();
