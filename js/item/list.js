231bb27be442002f523da2ed95e8c6cc38f352e7
seojungah <sjungah99@gmail.com>
2020-08-04 17:57:03 +0900

    2020-08-04

--- js/item/list.js
+++ js/item/list.js
@@ -3,32 +3,64 @@ const makeList = async () => {
     const list = document.querySelector("#list");
     if (document.querySelector("#list")) list.innerHTML = await fetchHtmlAsText("/list.html");
 
-    const listBtn = document.querySelectorAll(".box");
-    const listItem = [...listBtn];
 
-    const main = document.querySelector("#main");
-    const mainTitle = document.querySelector("#main > .info > h1");
-    const mainSub = document.querySelector("#main > .info > p");
+    //json
+    const listItem =document.querySelector("#list .swiper-wrapper");
 
-    const section = document.querySelector("section");
-    const closeBtn = document.querySelector(".close_btn");
-    const closeMain = document.getElementsByClassName("close");
+    const jsonToHtml = async (url) => {
+        const { projects } = await fetch(url).then(response => response.json()) || [];
+        const html = projects.map(({ id, clas, tag, title, year, text, goggleUrl}) => (
+            `<div id="${id}" class="swiper-slide">
+              <p class="tag">${tag}</p>
+              <div class="box ${clas}">
+                 <div class="title">
+                    <h2>${title}</h2>
+                    <p>${year}</p>
+                 </div>
+                 <img src="https://drive.google.com/uc?export=download&id=${goggleUrl}">
+              </div>
+            </div >`
+        ));
+        return listItem.innerHTML = html.join('');
+    };
 
-    listItem.forEach((value) => {
+    jsonToHtml('/js/item/projects.json')   
+    const boxImage = document.querySelector(".box img");
+    console.log(boxImage);
+    
+};
+
+
+
+const eventList = async () => {
+
+  //click show main
+  const box = document.querySelectorAll(".box");
+  const listBtn = [...box];
+  const closeMain = document.getElementsByClassName("close");
+
+  if(box.length > 0) {
+
+
+    listBtn.forEach((value) => {
         const titleStr = value.childNodes[1].childNodes[1].textContent;
 
         value.addEventListener('click', () => {
             if (closeMain.length > 0) {
-                main.classList.toggle('close');
-                closeBtn.classList.toggle('close');
-                section.classList.toggle('close');
+                document.querySelector("#main").classList.toggle('close');
+                document.querySelector(".close_btn").classList.toggle('close');
+                document.querySelector("section").classList.toggle('close');
             }
-
-            main.style.backgroundColor = '#e3c3c2';
-            mainTitle.innerHTML = titleStr;
-            mainSub.style.display = 'none';
+            document.querySelector("#main").style.backgroundColor = '#e3c3c2';
+            document.querySelector("#main > .info > h1").innerHTML = titleStr;
+            document.querySelector("#main > .info > p").style.display = 'none';
         });
     });
-};
+
+  };
+
+  console.log(listBtn.length);
+}
 
 makeList();
+eventList();
